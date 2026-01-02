60abbf59cd887f3c3ca39ae762e813716f6a0837
Nayandeep <nayandeepsen@gmail.com>
2026-01-02 18:09:41 +0530

    added normal vs critical code

---
+++ src/com/kodewala/exceptionhandling/NormalVsCriticalCode.java
@@ -0,0 +1,27 @@
+package com.kodewala.exceptionhandling;
+
+public class NormalVsCriticalCode {
+
+	public static void main(String[] args) {
+
+		int a = 10; // Normal Code
+		int b = 11; // Normal Code
+
+		// int c = (a + b) / 0; // Critical Code -> immediately program crashes at this
+		// line & execution stops
+
+		int c;
+
+		try {
+
+			c = (a + b) / 0;
+
+		} catch (Exception e) {
+			System.out.println(e); // does not stops execution
+		}
+
+		System.out.println("Bye"); // runs
+
+	}
+
+}
