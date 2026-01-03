df519b0300a2ed18d998d2c62dfe3cb3a5b6abdf
Nayandeep <nayandeepsen@gmail.com>
2026-01-03 11:08:40 +0530

    added multiple catch blocks

---
+++ src/com/kodewala/exceptionhandling/MultipleCatchs.java
@@ -0,0 +1,39 @@
+package com.kodewala.exceptionhandling;
+
+public class MultipleCatchs {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0;
+
+		int c;
+		int arr[] = { 5, 8, 2, 1 };
+
+		try {
+
+			c = a / b; // As soon as this line executes it throws exception & control goes to catch
+			System.out.println(arr[5]); // this will not execute
+
+		} catch (ArithmeticException e) {
+
+			System.out.println(e.getMessage());
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+
+			System.out.println(e.getMessage());
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		try {
+			c = 10 / 10;
+			System.out.println(arr[5]);
+		} catch (ArithmeticException | ArrayIndexOutOfBoundsException e) { // Collapsing catch exceptions
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
