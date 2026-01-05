dfcf27fda44c93c4c6915972f0639fc9aa836761
Nayandeep <nayandeepsen@gmail.com>
2026-01-05 13:34:41 +0530

    refined comments

--- src/com/kodewala/exceptionhandling/ThrowsKeywordCheckedExcp.java
+++ src/com/kodewala/exceptionhandling/ThrowsKeywordCheckedExcp.java
@@ -9,10 +9,15 @@ public class ThrowsKeywordCheckedExcp {
 		FileReader fr = new FileReader("test.txt");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// You must add throws IOException that calls readFile()
 		// public static void main(String[] args) throws IOException {}
 		// or must use try-catch block
+
+		// readFile(); even if you add -> public static void main(String[] args) throws
+		// IOException
+		// you must handle it through try-catch
+
 		try {
 			readFile();
 		} catch (IOException e) {
