2b5c1edbe765cce0f0014051ddcf530ccd4fe6de
Nayandeep <nayandeepsen@gmail.com>
2026-01-07 17:56:15 +0530

    added class to explain throw vs throw keyword

---
+++ src/com/kodewala/exceptionhandling/ThrowsVsThrow.java
@@ -0,0 +1,24 @@
+package com.kodewala.exceptionhandling;
+
+public class ThrowsVsThrow {
+
+	public static void m1() throws RuntimeException, Exception { // not handled but compiler satisfied , but those who
+																	// will call m1 should handle that
+		m2(); // since m2 throws checked exception compiler forces you to handle that
+	}
+
+	public static void m2() throws Exception {
+		throw new RuntimeException("m2 throws runtime exception");
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			m1(); // Since m1 throws checked exception -> Exception, we must handle this
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
